c028ffda3fc65d0665d2c8d82d295a6dbe15caf3
Taraska Rafał - Korpo <Rafal.Taraska@orange.com>
2023-01-10 16:18:06 +0100

    v1.1

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))    # 1
 
 class Config:
-   SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"    # 2
+   SECRET_KEY = os.environ.get("SECRET_KEY") or "asdvdasdasdc"    # 2
    SQLALCHEMY_DATABASE_URI = (                           # 3
            os.environ.get('DATABASE_URL') or
            'sqlite:///' + os.path.join(BASE_DIR, 'mikroblog.db')
